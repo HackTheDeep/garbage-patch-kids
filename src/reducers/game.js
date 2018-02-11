a7eda712e69b3cedbbd1e5cb90429cccd293baba
Chris Fairbanks <cfairbanks@gmail.com>
2018-02-11 10:26:37 -0500

    remove unused "add missing points" reducer case

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,20 +1,12 @@
 const initialState = {
-  screen: 'start',
+  screen: 'play',
   score: 0,
-  patchCount: 0,
-  missedCount: 0,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_POINTS':
       return { ...state, score: state.score + action.points }
-    case 'ADD_MISSED_POINTS':
-      if (action.endsInPatch) {
-        return {...state, patchCount: state.patchCount + action.points};
-      } else {
-        return {...state, missedCount: state.missedCount + action.points};
-      }
     case 'START_GAME':
       return { ...state, screen: 'play' }
     case 'END_GAME':
