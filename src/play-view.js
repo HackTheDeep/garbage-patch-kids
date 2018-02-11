e3d3de9a9524bbf8eea34c92cae90ce0e0a3c2cc
Chris Fairbanks <cfairbanks@gmail.com>
2018-02-10 23:43:28 -0500

    change canvas width/height into constants, draw a world map in the background of the canvas

--- src/play-view.js
+++ src/play-view.js
@@ -3,13 +3,16 @@ import { endGame } from './actions/game.js'
 import { fetchNewTrash } from "./actions/fetch-new-trash.js";
 import Game from './components/game.js'
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 430;
+
 const PlayView = ({ state, dispatch }) => {
   return (
     <div className='panel'>
-      <div onClick={() => dispatch(fetchNewTrash(600, 300))}>
+      <div onClick={() => dispatch(fetchNewTrash(CANVAS_WIDTH, CANVAS_HEIGHT))}>
         click me for a new trash element
       </div>
-      <Game time={state.time} dispatch={dispatch} trash={state.trash} mapWidth={600} mapHeight={300}/>
+      <Game time={state.time} dispatch={dispatch} trash={state.trash} mapWidth={CANVAS_WIDTH} mapHeight={CANVAS_HEIGHT}/>
       <button onClick={() => dispatch(endGame())}>End game</button>
     </div>
   )
